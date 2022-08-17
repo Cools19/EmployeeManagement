c7a54bcef946cb1d78d4d949c7fefa4546040875
unknown <cogjava3106@iiht.tech>
2022-08-17 13:42:46 +0530

    services till delete

--- src/main/java/com/emp/controller/EmployeeController.java
+++ src/main/java/com/emp/controller/EmployeeController.java
@@ -1,9 +1,18 @@
 package com.emp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emp.model.Employee;
 import com.emp.service.IEmployeeService;
 
 
@@ -19,4 +28,29 @@ public class EmployeeController {
 	 return "Hello world";
  }
 
+ @PostMapping("/employee")
+ Integer createEmployee(@RequestBody Employee employee ) {
+	 Integer id = employeeService.saveEmployee(employee);
+	 System.out.println(id);
+	 return id;
+ }
+ 
+ @GetMapping("/allemployees")
+ public List<Employee> getEmployees(){
+	 return employeeService.getAllEmployees();
+ }
+ 
+ @GetMapping("/getemployee/{id}")
+ public ResponseEntity<Employee> deleteEmployee(@PathVariable Integer id){
+	 System.out.println(id);
+	 ResponseEntity<Employee> responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			 try {
+				 employeeService.deleteEmployee(id);
+			 }
+	 		catch(Exception e) {
+	 			e.printStackTrace();
+	 			responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	 		}
+	 return responseEntity;
+ }
 }
